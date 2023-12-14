0358fb362393f468bb2436787767ddcbfe2882ed
luvjoshi2003 <149707062+luvjoshi2003@users.noreply.github.com>
2023-12-14 16:23:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 //initially vairables need????
 
 let oldTab = userTab;
-const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+const API_KEY = "7d18edeb1c1fd6280cdd39f130e611e4";
 oldTab.classList.add("current-tab");
 getfromSessionStorage();
 
